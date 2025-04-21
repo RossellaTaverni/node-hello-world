b725b7504f86082e90958af3ee359580d2b40615
Rossella <rossellataverni02@gmail.com>
2025-04-21 10:16:50 +0200

    [UPD] Update Bonus

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const param = process.argv[2];
+const param = process.argv[2] ? process.argv[2] : 'Boolean';
+// Utilizzando la formula '? process.argv[2] : 'Boolean'' andiamo a fare un controllo tramite l'operatore ternario
 
 console.log(`Hello ${param}`);
